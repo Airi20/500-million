7432f1d663477dc0595d816539a2be002fb0920b
Airi <aiai9004432221@gmail.com>
2025-06-29 00:34:26 +0900

    o

--- src/JumpGame.jsx
+++ src/JumpGame.jsx
@@ -23,7 +23,7 @@ export default function BillionYearsButton() {
         margin: "auto",
       }}
     >
-      <h1 style={{ fontSize: "20px" }}>🌀 5億年ボタン</h1>
+      <h1 style={{ fontSize: "20px" }}>🌀 行間調節って難しい</h1>
 
       {!started ? (
         <>
