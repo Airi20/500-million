6ec6b68d615c52a0a9ef84f0873fea05d3dd06f4
Airi <aiai9004432221@gmail.com>
2025-06-28 23:35:46 +0900

    f

--- src/App.js
+++ src/App.js
@@ -19,14 +19,15 @@ function App() {
         fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
       }}
     >
-      <h1 style={{ marginBottom: 8, fontSize: "1.1rem", lineHeight: 1.1 }}>
-        ５億年ボタン疑似体験 🎮
-      </h1>
+      <h1 style={{ marginBottom: 4, fontSize: "1.1rem", lineHeight: 1.1 }}>
+  ５億年ボタン疑似体験 🎮
+</h1>
 
-      <p style={{ fontSize: "0.75rem", marginBottom: 8, lineHeight: 1.2 }}>
-        5億年 = 約15,778,800,000,000,000秒<br />
-        （5億 × 365日 × 24時間 × 60分 × 60秒）
-      </p>
+<p style={{ fontSize: "0.75rem", marginBottom: 8, lineHeight: 1.2 }}>
+  5億年 = 約15,778,800,000,000,000秒<br />
+  （5億 × 365日 × 24時間 × 60分 × 60秒）
+</p>
+
 
       <JumpGame />
     </div>
